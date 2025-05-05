aba992323e28ec534a11eb2ae83479f7676f991d
janek <jan.karpinski@student.put.poznan.pl>
2025-05-05 15:26:59 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace GraLab6App
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnOpcje = new System.Windows.Forms.Button();
+            this.btnOpusc = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(74, 35);
+            this.label1.MaximumSize = new System.Drawing.Size(170, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(166, 126);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "GDZIE JEST DYDELF";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(81, 209);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(149, 67);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "START";
+            this.btnStart.UseVisualStyleBackColor = true;
+            // 
+            // btnOpcje
+            // 
+            this.btnOpcje.Location = new System.Drawing.Point(81, 282);
+            this.btnOpcje.Name = "btnOpcje";
+            this.btnOpcje.Size = new System.Drawing.Size(149, 64);
+            this.btnOpcje.TabIndex = 2;
+            this.btnOpcje.Text = "OPCJE";
+            this.btnOpcje.UseVisualStyleBackColor = true;
+            // 
+            // btnOpusc
+            // 
+            this.btnOpusc.Location = new System.Drawing.Point(81, 352);
+            this.btnOpusc.Name = "btnOpusc";
+            this.btnOpusc.Size = new System.Drawing.Size(149, 67);
+            this.btnOpusc.TabIndex = 3;
+            this.btnOpusc.Text = "OPUSC";
+            this.btnOpusc.UseVisualStyleBackColor = true;
+            this.btnOpusc.Click += new System.EventHandler(this.btnOpusc_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(313, 473);
+            this.Controls.Add(this.btnOpusc);
+            this.Controls.Add(this.btnOpcje);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnOpcje;
+        private System.Windows.Forms.Button btnOpusc;
+    }
+}
+
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("GraLab6App")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GraLab6App")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a1acbc3e-4341-4380-8e59-fae5d99b0542")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
